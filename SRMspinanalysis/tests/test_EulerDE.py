dae77a543f79c61289046450cfc7dd91a08be496
Matthew Morse <morsemat@oregonstate.edu>
2018-04-25 16:18:16 -0700

    Split test module into separate scripts for each module being tested.

---
+++ SRMspinanalysis/tests/test_EulerDE.py
@@ -0,0 +1,10 @@
+import EulerDE
+
+def test_compute_moments_1():
+    params = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0])
+    thrust_motor_1 = 25.0
+    thrust_motor_2 = 6.0
+    # Computed by hand
+    exp = np.array([0.0, 51.0, 37.0])
+    obs = EulerDE.compute_moments(params, thrust_motor_1, thrust_motor_2)
+    assert np.allclose(exp, obs)
